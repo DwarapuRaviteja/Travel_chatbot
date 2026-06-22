b08c1d96135ec1132f9d9e2c03682c28b3120f7b
Dwarapu Ravi Teja <ravitejadwarapu9@gmail.com>
2026-06-23 00:19:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -111,4 +111,10 @@ def new_chat():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=False
+    )
